a08e863eed2867deae6e473504bbf4ec9010e8ab
liushi <1842986895@qq.com>
2020-01-12 19:36:25 +0800

    项目基本配置

---
+++ src/redux/reducers.js
@@ -0,0 +1,23 @@
+/**
+ * 根据prevState和action生成newState的函数模块
+ */
+import { combineReducers } from 'redux';
+
+function aaa(prevState = 111, action) {
+  switch (action.type) {
+    default:
+      return prevState;
+  }
+}
+
+function bbb(prevState = 222, action) {
+  switch (action.type) {
+    default:
+      return prevState;
+  }
+}
+
+export default combineReducers({
+  aaa,
+  bbb
+});
