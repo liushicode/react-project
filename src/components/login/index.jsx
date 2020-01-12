160515a46a505cfb038a4d84e4bd5ad908e9d92d
liushi <1842986895@qq.com>
2020-01-13 00:14:53 +0800

    完成login页面表单注册

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,11 +1,110 @@
 import React, { Component } from 'react'
+//图片必须引入，才能被webpack打包
+import logo from './logo.png'
+import { Form, Icon, Input, Button } from "antd";
+import './index.less'
 
-export default class index extends Component {
+// @Form.create()//装饰器语法
+ class Login extends Component {
+  // 自定义表单校验规则
+   validator = (rule, value, callback) => {
+    console.log(rule,value);//表单的key和value
+    const name = rule.field === "username" ? "用户名" : "密码";
+
+    const reg = /^\w+$/;
+
+    if (!value) {
+      // 输入值为空
+      callback(`${name}不能为空`);
+    } else if (value.length < 4) {
+      callback(`${name}必须大于4位`);
+    } else if (value.length > 15) {
+      callback(`${name}必须小于15位`);
+    } else if (!reg.test(value)) {
+      callback(`${name}只能包含英文、数字、下划线`);
+    }
+    /*
+      callback() 调用不传参，代表表单校验成功
+      callback(message) 调用传参，代表表单校验失败，会提示message错误
+    */
+    // 必须要调用，否则会出问题
+    callback();
+  };
+
   render() {
+    // getFieldDecorator高阶组件：用来表单校验，先要通过Form.create()(Login)引入form属性
+    const { getFieldDecorator } = this.props.form;
+
     return (
-      <div>
-        Login...
+      <div className="login">
+        <header className="login-header">
+          <img src={logo} alt="logo" />
+          <h1>React项目: 后台管理系统</h1>
+        </header>
+        <section className="login-section">
+          <h3>用户登录</h3>
+          <Form className="login-form">
+            <Form.Item>
+              {getFieldDecorator("username", {
+                rules: [
+                  // {
+                  //   required: true,
+                  //   message: '用户名不能为空'
+                  // },
+                  // {
+                  //   min: 4,
+                  //   message: '用户名必须大于3位'
+                  // },
+                  // {
+                  //   max: 15,
+                  //   message: '用户名必须小于15位'
+                  // },
+                  // {
+                  //   pattern: /^\w+$/,
+                  //   message: '用户名只能包含英文、数字、下划线'
+                  // }
+
+                  {
+                    validator: this.validator
+                  }
+                ]
+              })(
+                <Input
+                  prefix={
+                    <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  placeholder="用户名"
+                />
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator("password", {
+                rules: [
+                  {
+                    validator: this.validator
+                  }
+                ]
+              })(
+                <Input
+                  prefix={
+                    <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  placeholder="密码"
+                />
+              )}
+            </Form.Item>
+            <Form.Item>
+              <Button className="login-form-btn" type="primary">
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+        </section>
       </div>
-    )
+    );
   }
 }
+
+// Form.create()(Login) 高阶组件：给Login传递form属性
+ export default Form.create()(Login);
+//export default Login;
